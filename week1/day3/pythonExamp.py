13e05dfb7b61e218c932f57aec60a89a9882ef59
Phillip Francis <phillipmfrancis93@yahoo.com>
2021-03-10 12:37:50 -0500

    noBacktTics

--- week1/day3/pythonExamp.py
+++ week1/day3/pythonExamp.py
@@ -1,7 +1,10 @@
 moneys = 0
 somethingToCallIt = ["Love", "Hate", "Anger"]
 somethingToCallIt = {"What it do": "nothing much",}
+moneysAgain = 3
+howMuchMoneys = moneys + moneysAgain
 print("Hello World")
 print("What it do babbbbbyyyyyyy")
 print(moneys)
 print(somethingToCallIt)
+print(moneysAgain)

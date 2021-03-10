222a3f5f10cd7b2e34f642017cad45e1c049f72e
Phillip Francis <phillipmfrancis93@yahoo.com>
2021-03-10 14:16:25 -0500

    inputPy

--- week1/day3/inputPython.py
+++ week1/day3/inputPython.py
@@ -1,4 +1,4 @@
 # name_of_user = input("What is your name?")
 nameOfUser = input("What is your name?")
 lastNameOfUser = input("What is your last name?")
-print("Hello %s, welcome to python" % (nameOfUser, lastNameOfUser))
+print("Hello %s %s, welcome to python" % (nameOfUser, lastNameOfUser))

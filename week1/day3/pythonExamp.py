3822a8fc8b5457d77a020a1e310a24235188fcfa
Phillip Francis <phillipmfrancis93@yahoo.com>
2021-03-10 12:28:57 -0500

    somethingCall

--- week1/day3/pythonExamp.py
+++ week1/day3/pythonExamp.py
@@ -1,5 +1,6 @@
 moneys = 0
 somethingToCallIt = ["Love", "Hate", "Anger"]
+somethingToCallIt = {"What it do": "nothing much",}
 print("Hello World")
 print("What it do babbbbbyyyyyyy")
 print(moneys)

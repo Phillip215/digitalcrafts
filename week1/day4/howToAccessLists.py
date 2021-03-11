bdf11e2b3bc740193d31e8c4399be8303861253f
Phillip Francis <phillipmfrancis93@yahoo.com>
2021-03-11 14:06:06 -0500

    more work

--- week1/day4/howToAccessLists.py
+++ week1/day4/howToAccessLists.py
@@ -4,3 +4,8 @@ pets = ["dog", "cat", "ham"]
 dog = pets[1]
 
 print(dog)
+
+# If you want to use a range of values in a list, you use a number
+# and a colon : and then end of the range you want to access
+# favs = pets[1: 2]
+# print(favs)

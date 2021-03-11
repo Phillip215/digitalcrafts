1033bbef4b1b4e58cd7ae98186354ba250f6013b
Phillip Francis <66640425+Phillip215@users.noreply.github.com>
2021-03-11 14:14:57 -0500

    functions and lists

---
+++ week1/day4/howToAccessLists.py
@@ -0,0 +1,11 @@
+pets = ["dog", "cat", "ham"]
+
+# We access list variables by their index, which starts at 0
+dog = pets[1]
+
+print(dog)
+
+# If you want to use a range of values in a list, you use a number
+# and a colon : and then end of the range you want to access
+# favs = pets[1: 2]
+# print(favs)
---
+++ week1/day4/lists.py
@@ -0,0 +1,13 @@
+# lists
+# contains stuff inside square brackets
+aList = ["monday", "Tuesday", "Wednesday"]
+
+# Dictionary
+#sequence of Labeled values
+diction = {
+  "name" : "joe", "age" : "37",
+  "height" : "6-1", "hairHeight" : "three feet"
+    "placesLived" : {
+      "US" : {"city" : "tallahasse"}
+    }
+}

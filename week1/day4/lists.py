20a125f45c593a3f174935f841a63e1bbaa037e3
Phillip Francis <phillipmfrancis93@yahoo.com>
2021-03-15 12:17:58 -0400

    lists

--- week1/day4/lists.py
+++ week1/day4/lists.py
@@ -6,7 +6,7 @@ aList = ["monday", "Tuesday", "Wednesday"]
 #sequence of Labeled values
 diction = {
   "name" : "joe", "age" : "37",
-  "height" : "6-1", "hairHeight" : "three feet"
+  "height" : "6-1", "hairHeight" : "three feet",
     "placesLived" : {
       "US" : {"city" : "tallahasse"}
     }

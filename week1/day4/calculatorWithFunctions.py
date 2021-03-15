64d6208ad3ab3d2ba8addd24fb810c21208eee2a
Phillip Francis <phillipmfrancis93@yahoo.com>
2021-03-15 17:29:13 -0400

    funky

--- week1/day4/calculatorWithFunctions.py
+++ week1/day4/calculatorWithFunctions.py
@@ -1,13 +1,32 @@
+def hello():
+  x = """
+    What up my boy I'm a calculator!
+    """
+
+  return print(x)
+
+def opperand(opper, firstNum, secondNum):
+  solution = ""
+  if opper == "+":
+    solution = int(firstNum) + int(secondNum)
+  elif opper == "-":
+    solution = int(firstNum) - int(secondNum)
+  elif opper == "/":
+    solution = int(firstNum) / int(secondNum)
+  elif opper == "*":
+    solution = int(firstNum) * int(secondNum)
+  else:
+    print("Invalid input")
+  return print(f"The answer is {solution}")
 
 
-
-
-
-
-def theWords():
-  print(int(input("I'm just your everyday calculator put in a number ")))
-
-
-
-
-theWords()
+options = ""
+hello()
+while (options != "y"):
+  first = input("What's your first number? ")
+  opp = input("Now please put in a opperand ")
+  second = input("Now the second number please ")
+  opperand(opp, first, second)
+  exit = input("Are you done my boy! (press y for yes) or (press anything else for no)")
+  if exit == "y":
+    break

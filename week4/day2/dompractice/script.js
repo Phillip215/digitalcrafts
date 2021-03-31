a3fd4afb7f85f8f37b3759f36d5f331150c74642
Phillip Francis <phillipmfrancis93@yahoo.com>
2021-03-31 17:00:08 -0400

    stuff

---
+++ week4/day2/dompractice/script.js
@@ -0,0 +1,9 @@
+console.log("Hello")
+
+function ResultsDisplay() {
+  const displayInput = document.querySelector(".input").value
+  console.log(displayInput)
+
+  const target = document.querySelector("displayNumber")
+
+}
---
+++ week4/day2/makeajscriptsite/script.js
@@ -0,0 +1,20 @@
+const contain = document.querySelector("#main-container")
+console.log(contain)
+
+const listContain = document.createElement("ul")
+console.log(listContain)
+
+const listItemOne = document.createElement('li')
+console.log(listItemOne)
+const listItemTwo = document.createElement('li')
+console.log(listItemTwo)
+const listItemThree = document.createElement('li')
+console.log(listItemThree)
+const listItemFour = document.createElement('li')
+console.log(listItemTwo)
+const listItemFive = document.createElement('li')
+console.log(listItemThree)
+
+const put = document.createElement("input")
+console.log(put)
+put.type = "radio"

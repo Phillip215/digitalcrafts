00798a3f67439b532861a6749743475466da1477
Phillip Francis <phillipmfrancis93@yahoo.com>
2021-04-01 10:30:31 -0400

    yup

--- week4/day2/tipcalc/script.js
+++ week4/day2/tipcalc/script.js
@@ -4,7 +4,7 @@ tipsy.addEventListener("click", function() {
   let bill = document.getElementById("bill-amount").value;
   let tipPercentage = document.getElementById("tip-percentage").value;
 
-  let tipAmount = document.getElementById("tip-amount").value = bill / tipPercentage;
+  let tipAmount = document.getElementById("tip-amount").value = bill * tipPercentage;
   let total = document.getElementById("total").value = parseFloat(bill) + parseFloat(tipAmount);
 
   let un = document.querySelector("#unorder")

98fed7dda807e7cd209864b4c6f8ecba817ea9b7
Phillip Francis <phillipmfrancis93@yahoo.com>
2021-04-30 14:33:40 -0400

    db update

--- week8/day5/db.sql
+++ week8/day5/db.sql
@@ -17,5 +17,10 @@ CREATE TABLE patients(
 CREATE TABLE appointments(
   appointment_id SERIAL PRIMARY KEY,
   procedure VARCHAR(50),
-  date VARCHAR(8)
+  date VARCHAR(8),
+  FOREIGN KEY(doctor_id),
+  FOREIGN KEY(patient_id),
+  REFERENCES doctors(doctor_id),
+  REFERENCES patients(patient_id),
+  ON DELETE CASCADE
 )

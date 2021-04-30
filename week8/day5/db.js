89ef76a10e2656224b996079633dc5d36d94618d
Phillip Francis <phillipmfrancis93@yahoo.com>
2021-04-30 11:14:05 -0400

    backend work

---
+++ week8/day5/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool
+
+const pool = new Pool({
+  user: "postgres",
+  password: "Mongoose",
+  host: "localhost",
+  port: 5432,
+  database: "hospital"
+});
+
+module.exports = pool;
---
+++ week8/day5/db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE hospital;
+
+CREATE TABLE doctors(
+  doctor_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(20),
+  last_name VARCHAR(20),
+  profession VARCHAR(30)
+)
+
+CREATE TABLE patients(
+  patient_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(20),
+  last_name VARCHAR(20),
+  condition VARCHAR(50)
+)
+
+CREATE TABLE appointments(
+  appointment_id SERIAL PRIMARY KEY,
+  procedure VARCHAR(50),
+  date VARCHAR(8)
+)
